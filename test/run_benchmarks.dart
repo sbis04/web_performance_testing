a9cebfca687704f50b35522c63e14948928f9204
Souvik Biswas <sbis1999@gmail.com>
2020-11-08 23:00:22 +0530

    Report generation

--- test/run_benchmarks.dart
+++ test/run_benchmarks.dart
@@ -1,4 +1,5 @@
 import 'dart:convert' show JsonEncoder;
+import 'dart:io';
 
 import 'package:web_benchmarks_framework/server.dart';
 
@@ -8,5 +9,10 @@ Future<void> main() async {
     entryPoint: 'lib/benchmarks/runner.dart',
     useCanvasKit: false,
   );
-  print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
+  // print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
+
+  final result = File('report.json').openWrite(mode: FileMode.write);
+  result.write(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
+
+  await result.close();
 }
